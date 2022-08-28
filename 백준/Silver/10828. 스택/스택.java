c58a5072a527c4111809f8c7603321155327f71e
Yuujin <97090471+you-jin12@users.noreply.github.com>
2022-08-28 22:34:05 +0900

    [Silver IV] Title: 스택, Time: 388 ms, Memory: 18780 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -2,62 +2,62 @@ import java.util.*;
 import java.io.*;
 
 public class Main{
-    public static int[] stack;
-    public static int cur=0;
-    public static void main(String[] args)throws Exception{
+    static int[] stack=new int[10000];
+    static int peak=0;
+    public static void main(String[] args)throws IOException{
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         int n=Integer.parseInt(br.readLine());
-        StringBuilder sb=new StringBuilder();
-        stack=new int[n];
-        
+        int number;
         for(int i=0;i<n;i++){
             StringTokenizer st=new StringTokenizer(br.readLine());
-            String str=st.nextToken();
+            String op=st.nextToken();
             
-            switch(str){
-                case "push" :
-                    push(Integer.parseInt(st.nextToken()));
-                    break;
+            switch(op){
+                case "push":
+                    number=Integer.parseInt(st.nextToken());
+                    push(number); break;
                 case "pop":
-                    pop();
-                    break;
+                    pop(); break;
                 case "size":
-                    size();
-                    break;
+                    size(); break;
                 case "empty":
-                    empty();
-                    break;
+                    empty(); break;
                 case "top":
-                    top();
+                    top(); break;
             }
         }
     }
-    public static void push(int data){
-        stack[cur]=data;
-        cur++;
+    public static void push(int num){
+        stack[peak]=num; peak++;
     }
+    
     public static void pop(){
-        if(cur== 0){
+        if(peak==0){
             System.out.println(-1);
         }
         else{
-            cur--;
-            System.out.println(stack[cur]);
+            peak=peak-1;
+            System.out.println(stack[peak]);
         }
     }
+    
     public static void size(){
-        System.out.println(cur);
+        System.out.println(peak);
     }
+    
     public static void empty(){
-        if(cur==0){
+        if(peak==0){
             System.out.println(1);
         }
-        else System.out.println(0);
+        else
+            System.out.println(0);
     }
+    
     public static void top(){
-        if(cur==0){
+        if(peak==0){
             System.out.println(-1);
         }
-        else System.out.println(stack[cur-1]);
+        else 
+            System.out.println(stack[peak-1]);
     }
 }
