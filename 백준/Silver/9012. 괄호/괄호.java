72952f7bf91b326f570273c04d4da8db11e9cb8b
Yuujin <97090471+you-jin12@users.noreply.github.com>
2022-08-29 21:28:06 +0900

    [Silver IV] Title: 괄호, Time: 136 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,36 +1,35 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Stack;
+import java.util.*;
+import java.io.*;
 
-public class Main {
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
-		
-		for(int i = 0; i < T; i++) {
-			String S = br.readLine();
-			
-			
-			Stack<Character> stack = new Stack<Character>();
-			
-			for(int j = 0; j < S.length(); j++) {
-				if(S.charAt(j) == '(') {
-					stack.push(S.charAt(j));
-				}else {
-
-					if(stack.empty()) {
-						stack.push(S.charAt(j));
-						break;
-					}else {
-						stack.pop();
-					}
-				}
-			}
-			if(stack.empty()) System.out.println("YES");
-			else System.out.println("NO");
-		}
-	}
-
+public class Main{
+    public static void main(String[] args)throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int test=Integer.parseInt(br.readLine());
+        char c;
+        Stack<Character> stack=new Stack<>();
+        for(int i=0;i<test;i++){
+            String str=br.readLine();
+            for(int j=0;j<str.length();j++){
+                c=str.charAt(j);
+                if(c=='('){
+                    stack.push(c);
+                }
+                else{
+                    if(stack.empty()){
+                        stack.push(c);
+                        break;
+                    }
+                    else
+                        stack.pop();
+                }
+            }
+            if(stack.empty()){
+                System.out.println("YES");
+            }
+            else{
+                System.out.println("NO");
+            }
+            stack.clear();
+         }
+    }
 }
