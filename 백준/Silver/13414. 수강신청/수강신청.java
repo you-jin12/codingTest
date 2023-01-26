b46d8df9fd973ebd3c8afb1901df4475b4abd41b
Yuujin <97090471+you-jin12@users.noreply.github.com>
2023-01-26 21:30:24 +0900

    [Silver III] Title: 수강신청, Time: 1360 ms, Memory: 67704 KB -BaekjoonHub

---
+++ 백준/Silver/13414. 수강신청/수강신청.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args)throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine());
+        
+        int k=Integer.parseInt(st.nextToken());//수강가능인원
+        int l=Integer.parseInt(st.nextToken()); //대기목록 길이
+        
+        LinkedHashSet<String> map=new LinkedHashSet<>();
+        for(int i=0;i<l;i++){
+            String student=br.readLine();
+            if(map.contains(student)){
+                map.remove(student);
+            }
+            map.add(student);
+        }
+        Iterator<String> iter=map.iterator();
+        
+        for(int i=0;i<k;i++){
+            if(iter.hasNext()){
+                System.out.println(iter.next());
+            }
+        }
+    }
+}
