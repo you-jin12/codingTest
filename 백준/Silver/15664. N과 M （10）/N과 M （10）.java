d96635b83080902e0eb570c960f64f5399bf07cf
yyujjin <97090471+you-jin12@users.noreply.github.com>
2025-09-29 18:30:36 +0900

    [Silver II] Title: N과 M (10), Time: 124 ms, Memory: 15816 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,52 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int n, m;
+    static int[] arr, result;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n];
+        result = new int[m];
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+        recursion(0, 0);
+        System.out.print(sb);
+
+    }
+
+    public static void recursion(int start, int count) {
+
+        if (count == m) {
+            for (int num : result) {
+                sb.append(num + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        int prev = -1;
+        for (int i = start; i < n; i++) {
+            if (prev == arr[i]) {
+                continue;
+            }
+            result[count] = arr[i];
+            prev = arr[i];
+            recursion(i + 1, count + 1);
+        }
+    }
+
+}
